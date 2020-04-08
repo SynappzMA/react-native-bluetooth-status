e4faac791716af5b536cef81a256e0a0534d1913
Wouter van den Broek <woutervdbroek@gmail.com>
2020-04-08 17:17:18 +0200

    AndroidX compatible for React Native 0.60 and above

--- android/src/main/java/com/solinor/bluetoothstatus/RNBluetoothManagerModule.java
+++ android/src/main/java/com/solinor/bluetoothstatus/RNBluetoothManagerModule.java
@@ -6,7 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Handler;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.LifecycleEventListener;
@@ -15,7 +15,6 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
